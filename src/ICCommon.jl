c8f716cdb40d0109f753ada8a1ceee7bb0de5ad8
Andrew Keller <andrew.keller.09@gmail.com>
2016-10-07 15:11:03 -0700

    Initial commit

--- src/ICCommon.jl
+++ src/ICCommon.jl
@@ -1,5 +1,21 @@
 module ICCommon
 
-# package code goes here
+export PlotSetup, PlotPoint
 
-end # module
+type PlotSetup
+    arrtype::DataType
+    size::Tuple
+    kwargs::Dict
+end
+
+function PlotSetup(a,b; kwargs...)
+    argdict = Dict(k=>v for (k,v) in kwargs)
+    PlotSetup(a,b,argdict)
+end
+
+type PlotPoint
+    inds
+    v
+end
+
+end
